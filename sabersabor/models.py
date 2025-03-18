e8e0f9dac4f9e5cbc5d0077599e76030e319bca0
brian denz <67394688+briandenz@users.noreply.github.com>
2025-03-18 18:17:44 -0400

    Update models.py
    Removed duplicate db SQLAlchemy instance

--- sabersabor/models.py
+++ sabersabor/models.py
@@ -1,7 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy 
 from datetime import date 
-
-db = SQLAlchemy()
+from sabersabor.db import db
 
 #Recipe-Ingredient many-to-many association table
 recipe_ingredients = db.Table(
